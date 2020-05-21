356a0bd1c046da623af6296e7735805a63e1dacd
gguilt <58526116+gguilt@users.noreply.github.com>
2020-05-21 07:36:57 +0300

    Demo version

---
+++ js/danse-mouse.js
@@ -0,0 +1,60 @@
+// TODO: Major clean-up
+// TODO: Make it more humane and make it get harder better -faster stronger-
+
+let canvas = document.getElementById("mainCanvas");
+const ctx = canvas.getContext("2d");
+
+let box = {
+	x: 20, y: 20,
+	dxm: 1, dym: 1,
+	width: 20, height: 20
+};
+
+let score = 0;
+
+function main() {
+	// Updating
+	canvas.width = (window.innerWidth > 800) ? window.innerWidth : 800;
+	canvas.height = (window.innerHeight > 500) ? window.innerHeight : 500;
+
+	box.width = (Math.floor(Math.random() * (100 - score)) + 10);
+	box.height = (Math.floor(Math.random() * (100 - score)) + 10);
+	box.x += (score+1) * 0.5 * box.dxm * (Math.random() * 5 + 1);
+	box.y += (score+1) * 0.5 * box.dym * (Math.random() * 5 + 1);
+
+	if (box.x <= 0 || box.x + box.width >= canvas.width) {
+		if (box.x <= 0) box.x = 0;
+		else if (box.x + box.width >= canvas.width) box.x = canvas.width - box.width;
+		box.dxm = -box.dxm;
+	}
+
+	if (box.y <= 0 || box.y + box.height >= canvas.height) {
+		if (box.y <= 0) box.y = 0;
+		else if (box.y + box.height >= canvas.height) box.y = canvas.height - box.height;
+		box.dym = -box.dym;
+	}
+
+	// Event handling
+	canvas.addEventListener("click", function(event) {
+		mouseX = event.clientX - canvas.getBoundingClientRect().left;
+		mouseY = event.clientY - canvas.getBoundingClientRect().top;
+
+		if (mouseX >= box.x && mouseX <= box.x + box.width &&
+			mouseY >= box.y && mouseY <= box.y + box.height) {
+			score++;
+			box.x = (Math.random() * (canvas.width - box.width - 5) + 1);
+			box.y = (Math.random() * (canvas.height - box.height - 5) + 1);
+		}
+	})
+
+	// Drawing
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "red";
+	ctx.beginPath();
+	ctx.fillRect(box.x, box.y, box.width, box.height);
+	ctx.stroke();
+
+	ctx.font = "24px serif";
+	ctx.strokeText(score + " Points", 5, 30);
+}
+setInterval(main, 10);
